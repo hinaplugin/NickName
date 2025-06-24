ca89d40f834c7d4050cbaf1272fbac39c892ce41
hina2113 <110330118+hinaplugin@users.noreply.github.com>
2025-06-25 01:29:40 +0900

    権限チェックを追加

--- src/main/java/com/hinaplugin/nickName/Commands.java
+++ src/main/java/com/hinaplugin/nickName/Commands.java
@@ -32,90 +32,106 @@ public class Commands implements CommandExecutor, TabCompleter {
             final String subCommand = strings[0];
             switch (subCommand){
                 case "set" -> {
-                    if (strings.length != 2){
-                        player.sendMessage(miniMessage.deserialize("<red>usage: /nick set <nickname>"));
-                        return true;
-                    }
-                    if (strings[1].isEmpty()){
-                        player.sendMessage(miniMessage.deserialize("<red>ニックネームは1文字以上必要です．"));
-                        return true;
-                    }
-                    final Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
-                    final Team team = scoreboard.getPlayerTeam(player);
-                    if (team == null){
-                        player.displayName(Component.text(strings[1]));
-                        player.playerListName(Component.text(strings[1]));
+                    if (player.hasPermission("nick.commands.set")){
+                        if (strings.length != 2){
+                            player.sendMessage(miniMessage.deserialize("<red>usage: /nick set <nickname>"));
+                            return true;
+                        }
+                        if (strings[1].isEmpty()){
+                            player.sendMessage(miniMessage.deserialize("<red>ニックネームは1文字以上必要です．"));
+                            return true;
+                        }
+                        final Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+                        final Team team = scoreboard.getPlayerTeam(player);
+                        if (team == null){
+                            player.displayName(Component.text(strings[1]));
+                            player.playerListName(Component.text(strings[1]));
+                        }else {
+                            player.displayName(team.prefix().append(Component.text(strings[1])).append(team.suffix()).color(team.color()));
+                            player.playerListName(team.prefix().append(Component.text(strings[1])).append(team.suffix()).color(team.color()));
+                        }
+                        player.sendMessage(miniMessage.deserialize("<green>ニックネームを[" + strings[1] + "]に設定しました．"));
                     }else {
-                        player.displayName(team.prefix().append(Component.text(strings[1])).append(team.suffix()).color(team.color()));
-                        player.playerListName(team.prefix().append(Component.text(strings[1])).append(team.suffix()).color(team.color()));
+                        player.sendMessage(miniMessage.deserialize("<red>このコマンドを使用する権限がありません．"));
                     }
-                    player.sendMessage(miniMessage.deserialize("<green>ニックネームを[" + strings[1] + "]に設定しました．"));
                 }
                 case "setother" -> {
-                    if (strings.length != 3){
-                        player.sendMessage(miniMessage.deserialize("<red>usage: /nick setother <player> <nickname>"));
-                        return true;
-                    }
-                    if (strings[1].isEmpty()){
-                        player.sendMessage(miniMessage.deserialize("<red>usage: /nick setother <player> <nickname>"));
-                        return true;
-                    }
-                    final Player target = Bukkit.getPlayer(strings[1]);
-                    if (target == null){
-                        player.sendMessage(miniMessage.deserialize("<red>" + strings[1] + "が見つかりませんでした．"));
-                        return true;
-                    }
-                    if (strings[2].isEmpty()){
-                        player.sendMessage(miniMessage.deserialize("<red>ニックネームは1文字以上必要です．"));
-                        return true;
-                    }
-                    final Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
-                    final Team team = scoreboard.getPlayerTeam(target);
-                    if (team == null){
-                        target.displayName(Component.text(strings[2]));
-                        target.playerListName(Component.text(strings[2]));
+                    if (player.hasPermission("nick.commands.setother")){
+                        if (strings.length != 3){
+                            player.sendMessage(miniMessage.deserialize("<red>usage: /nick setother <player> <nickname>"));
+                            return true;
+                        }
+                        if (strings[1].isEmpty()){
+                            player.sendMessage(miniMessage.deserialize("<red>usage: /nick setother <player> <nickname>"));
+                            return true;
+                        }
+                        final Player target = Bukkit.getPlayer(strings[1]);
+                        if (target == null){
+                            player.sendMessage(miniMessage.deserialize("<red>" + strings[1] + "が見つかりませんでした．"));
+                            return true;
+                        }
+                        if (strings[2].isEmpty()){
+                            player.sendMessage(miniMessage.deserialize("<red>ニックネームは1文字以上必要です．"));
+                            return true;
+                        }
+                        final Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+                        final Team team = scoreboard.getPlayerTeam(target);
+                        if (team == null){
+                            target.displayName(Component.text(strings[2]));
+                            target.playerListName(Component.text(strings[2]));
+                        }else {
+                            target.displayName(team.prefix().append(Component.text(strings[2])).append(team.suffix()).color(team.color()));
+                            target.playerListName(team.prefix().append(Component.text(strings[2])).append(team.suffix()).color(team.color()));
+                        }
+                        player.sendMessage(miniMessage.deserialize("<green>" + strings[1] + "のニックネームを[" + strings[1] + "]に設定しました．"));
                     }else {
-                        target.displayName(team.prefix().append(Component.text(strings[2])).append(team.suffix()).color(team.color()));
-                        target.playerListName(team.prefix().append(Component.text(strings[2])).append(team.suffix()).color(team.color()));
+                        player.sendMessage(miniMessage.deserialize("<red>このコマンドを使用する権限がありません．"));
                     }
-                    player.sendMessage(miniMessage.deserialize("<green>" + strings[1] + "のニックネームを[" + strings[1] + "]に設定しました．"));
                 }
                 case "unset" -> {
-                    final Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
-                    final Team team = scoreboard.getPlayerTeam(player);
-                    if (team == null){
-                        player.displayName(Component.text(player.getName()));
-                        player.playerListName(Component.text(player.getName()));
+                    if (player.hasPermission("nick.commands.unset")){
+                        final Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+                        final Team team = scoreboard.getPlayerTeam(player);
+                        if (team == null){
+                            player.displayName(Component.text(player.getName()));
+                            player.playerListName(Component.text(player.getName()));
+                        }else {
+                            player.displayName(team.prefix().append(Component.text(player.getName())).append(team.suffix()).color(team.color()));
+                            player.playerListName(team.prefix().append(Component.text(player.getName())).append(team.suffix()).color(team.color()));
+                        }
+                        player.sendMessage(miniMessage.deserialize("<green>ニックネームをリセットしました．"));
                     }else {
-                        player.displayName(team.prefix().append(Component.text(player.getName())).append(team.suffix()).color(team.color()));
-                        player.playerListName(team.prefix().append(Component.text(player.getName())).append(team.suffix()).color(team.color()));
+                        player.sendMessage(miniMessage.deserialize("<red>このコマンドを使用する権限がありません．"));
                     }
-                    player.sendMessage(miniMessage.deserialize("<green>ニックネームをリセットしました．"));
                 }
                 case "unsetother" -> {
-                    if (strings.length != 2){
-                        player.sendMessage(miniMessage.deserialize("<red>usage: /nick unsetother <player>"));
-                        return true;
-                    }
-                    if (strings[1].isEmpty()){
-                        player.sendMessage(miniMessage.deserialize("<red>usage: /nick unsetother <player>"));
-                        return true;
-                    }
-                    final Player target = Bukkit.getPlayer(strings[1]);
-                    if (target == null){
-                        player.sendMessage(miniMessage.deserialize("<red>" + strings[1] + "が見つかりませんでした．"));
-                        return true;
-                    }
-                    final Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
-                    final Team team = scoreboard.getPlayerTeam(target);
-                    if (team == null){
-                        target.displayName(Component.text(target.getName()));
-                        target.playerListName(Component.text(target.getName()));
+                    if (player.hasPermission("nick.commands.unsetother")){
+                        if (strings.length != 2){
+                            player.sendMessage(miniMessage.deserialize("<red>usage: /nick unsetother <player>"));
+                            return true;
+                        }
+                        if (strings[1].isEmpty()){
+                            player.sendMessage(miniMessage.deserialize("<red>usage: /nick unsetother <player>"));
+                            return true;
+                        }
+                        final Player target = Bukkit.getPlayer(strings[1]);
+                        if (target == null){
+                            player.sendMessage(miniMessage.deserialize("<red>" + strings[1] + "が見つかりませんでした．"));
+                            return true;
+                        }
+                        final Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+                        final Team team = scoreboard.getPlayerTeam(target);
+                        if (team == null){
+                            target.displayName(Component.text(target.getName()));
+                            target.playerListName(Component.text(target.getName()));
+                        }else {
+                            target.displayName(team.prefix().append(Component.text(player.getName())).append(team.suffix()).color(team.color()));
+                            target.playerListName(team.prefix().append(Component.text(player.getName())).append(team.suffix()).color(team.color()));
+                        }
+                        player.sendMessage(miniMessage.deserialize("<green>" + strings[1] + "のニックネームをリセットしました．"));
                     }else {
-                        target.displayName(team.prefix().append(Component.text(player.getName())).append(team.suffix()).color(team.color()));
-                        target.playerListName(team.prefix().append(Component.text(player.getName())).append(team.suffix()).color(team.color()));
+                        player.sendMessage(miniMessage.deserialize("<red>このコマンドを使用する権限がありません．"));
                     }
-                    player.sendMessage(miniMessage.deserialize("<green>" + strings[1] + "のニックネームをリセットしました．"));
                 }
             }
         }else {
